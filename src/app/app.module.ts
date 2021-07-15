1fb23ed5e00f6b0b5a44933d365afe7eddd247b1
JE72539 <sundaramanikandan.kandaswamy@kbc.be>
2021-07-15 10:17:59 +0530

    updated demo library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { DemoLibModule} from 'demo-lib';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DemoLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
